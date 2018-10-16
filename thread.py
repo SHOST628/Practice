704e792eacb62cca711fca9f60d60ce2172c52f1
Sincave <599531369@qq.com>
2018-10-16 18:16:28 +0800

    Merge remote-tracking branch 'origin/master'
    practice threading

--- thread.py
+++ thread.py
@@ -12,10 +12,16 @@ def game(name):
     print("%s stop playing game %s"%(name,time.ctime()))
 
 if __name__ == "__main__":
+    Threadl = []
     Thread1 = threading.Thread(target=music,args=('SZ',))
     Thread2 = threading.Thread(target=game,args=('SZ',))
-    Thread1.start()
-    Thread2.start()
+    # Thread1.start()
+    # Thread2.start()
+    Threadl.append(Thread1)
+    Threadl.append(Thread2)
+    for i in Threadl:
+        i.start()
+    Thread1.join()
     print("Ending at %s"%(time.ctime()))
 
 
