c5f2a83a50adcf5d3fb7290db8c160e77dadf9d5
Sincave <599531369@qq.com>
2018-10-19 18:28:08 +0800

    Merge remote-tracking branch 'origin/master' practice threading new thread2.py , thread3.py and mutiprocessing.py

--- thread4.py
+++ thread4.py
@@ -1,21 +1,26 @@
 import threading
 from time import sleep
 
-num = 0
+thread_local =threading.local()
 # lock = threading.RLock()
+
+def show():
+    print(threading.current_thread().getName()," ",thread_local.num)
 
 def func(arg):
     # lock.acquire()
-    global num
-    sleep(1)
-    num +=1
-    print(num)
+    thread_local.num = arg
+    for i in range(10):
+        thread_local.num += 1
+    show()
     # lock.release()
+if __name__ == "__main__":
+    for i in range(2):
+        t = threading.Thread(target=func,args=(i,))
+        t.start()
+        # print(t.isAlive())
+        # t.join()
+    print("end")
 
-for i in range(10):
-    t = threading.Thread(target=func,args=(i,))
-    t.start()
-    # t.join()
-print("end")
 
 
